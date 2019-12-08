5d52ed90281866be0be23fceb0fa5f52446f55bd
komangyogananda <yoganandamahaputra@gmail.com>
2019-12-08 19:13:22 +0700

    specular + remove unused files

--- fragment.glsl
+++ fragment.glsl
@@ -20,11 +20,27 @@ void main() {
   //  dot product dari vektor arah datang cahaya • arah vektor normal
   float normalDotLight = max(dot(fNormal, diffuseDirection), 0.0);
 
+  float specularPower = 120.0;
+  float specular = 0.0;
+
+  if (normalDotLight > 0.0) {
+    // viewing vector
+    vec3 viewVec = vec3(0,0,1.0);
+
+    // reflective vector
+    vec3 reflectVec = reflect(-diffuseDirection, fNormal);
+
+    // determine the specularFactor based on the dot product of viewing and reflective,
+    // taking at least a minimum of 0.0
+    float specularFactor = max(dot(reflectVec, viewVec), 0.0);
+    specular = pow(specularFactor, specularPower);
+  }
+
   // Untuk mendapatkan nilai warna (RGBA) dari tekstur
   vec4 textureColor = texture2D(sampler0, fTexCoord);
 
   vec3 diffuse = diffuseColor * textureColor.rgb * normalDotLight;
   vec3 ambient = ambientColor * textureColor.rgb;
 
-  gl_FragColor = vec4(diffuse + ambient + fColor, 1.0);
+  gl_FragColor = vec4(diffuse + ambient + fColor + specular, 1.0);
 }
